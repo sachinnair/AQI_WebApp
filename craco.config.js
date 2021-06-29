46ec36105ef68502db9ca9f9b1882e0828c6465c
S <sachinkn6@gmail.com>
2021-06-29 13:47:27 +0530

    build improvements

--- craco.config.js
+++ craco.config.js
@@ -1,3 +1,5 @@
+const TerserPlugin = require("terser-webpack-plugin");
+
 // craco.config.js
 module.exports = {
   style: {
@@ -8,4 +10,56 @@ module.exports = {
       ],
     },
   },
+  webpack: {
+    configure: (webpackConfig, {env, paths}) => {
+      const modWebPackConfig = { ...webpackConfig };
+
+      
+      const newTerserOpts = new TerserPlugin({
+          terserOptions: {
+            parse: {
+              // We want terser to parse ecma 8 code. However, we don't want it
+              // to apply any minification steps that turns valid ecma 5 code
+              // into invalid ecma 5 code. This is why the 'compress' and 'output'
+              // sections only apply transformations that are ecma 5 safe
+              // https://github.com/facebook/create-react-app/pull/4234
+              ecma: 8,
+            },
+            compress: {
+              ecma: 5,
+              warnings: false,
+              // Disabled because of an issue with Uglify breaking seemingly valid code:
+              // https://github.com/facebook/create-react-app/issues/2376
+              // Pending further investigation:
+              // https://github.com/mishoo/UglifyJS2/issues/2011
+              comparisons: false,
+              // Disabled because of an issue with Terser breaking valid code:
+              // https://github.com/facebook/create-react-app/issues/5250
+              // Pending further investigation:
+              // https://github.com/terser-js/terser/issues/120
+              inline: 2,
+              drop_console: true,
+            },
+            mangle: {
+              safari10: true,
+            },
+            // Added for profiling in devtools
+            keep_classnames: process.argv.includes('--profile'),
+            keep_fnames: process.argv.includes('--profile'),
+            output: {
+              ecma: 5,
+              comments: false,
+              // Turned on because emoji and regex is not minified properly using default
+              // https://github.com/facebook/create-react-app/issues/2488
+              ascii_only: true,
+            },
+          },
+          sourceMap: false,
+        })
+
+      modWebPackConfig.optimization.minimizer[0] = newTerserOpts;
+
+      return modWebPackConfig;
+    }
+  }
 }
